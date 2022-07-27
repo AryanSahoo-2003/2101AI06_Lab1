60a1c7cbc73226348d37c632d819dd09476fc3d8
Aryansahoo-2003 <aryansahoo2021@gmail.com>
2022-07-27 20:35:44 +0530

    Input addition operation

--- calculator.c
+++ calculator.c
@@ -7,5 +7,17 @@ int main()
     printf("Please enter the first number and second number with a space : ");
     scanf("%lf",&a);
     scanf("%lf",&b);
+    printf("Choose from the following operations to be applied :- '+' ");
+    scanf(" %c", &c);
+    switch (c)
+    {
+    case '+':
+      result=a+b;
+      break;
+    default:
+    printf("You have not chosen appropriate operation");
+      break;
+    };
+    printf("The application of operation %c on two numbers yield result : %.3lf",c,result);
     return 0;
  }
