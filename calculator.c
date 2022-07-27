0e42ced75ed8508e5c397c92d350e22066b36034
Aryansahoo-2003 <aryansahoo2021@gmail.com>
2022-07-27 20:27:31 +0530

    Taking inputs for calculator

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+ {
+    double a,b;
+    char c;
+    double result;
+    printf("Please enter the first number and second number with a space : ");
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    return 0;
+ }
